b18a6708345f2ccdc19359011020ef0b7a438938
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-12 06:20:13 +0200

    Install Entity Framework

---
+++ Core/Application/Entities/Interfaces/IDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using WeatherForecastApp.Domain.Entities.Models;
+
+namespace WeatherForecastApp.Application.Entities.Interfaces
+{
+    public interface IDbContext
+    {
+        public DbSet<WeatherForecast> Products { get; set; }
+
+        public Task<int> SaveChanges();
+    }
+}
---
+++ Core/Domain/Entities/Models/WeatherForecast.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WeatherForecastApp.Domain.Entities.Models
+{
+    public sealed class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public float TemperatureInC { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
