17f04d17e3e27b186e4c35dbdbfcc5bf65b6a8d3
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-12 03:20:29 +0200

    Init and configure Web API .NET 8.0 project

---
+++ Presentation/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
