7c54d7acddbddca511a3c11a4a5d29786c4785d2
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-12 06:53:04 +0200

    First implementations of WeatherForecastContext in Persistence layer

---
+++ Core/Application/Entities/Interfaces/IDomainContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WeatherForecastApp.Application.Entities.Interfaces
+{
+    /// <summary>
+    /// The generic representation of a database context.
+    /// </summary>
+    /// <typeparam name="TEntity">The type of a database entity.</typeparam>
+    public interface IDomainContext<TEntity> where TEntity : class
+    {
+        /// <summary>
+        /// Gets or sets the entities from a database.
+        /// </summary>
+        public DbSet<TEntity> Entities { get; set; }
+
+        /// <summary>
+        /// Saves the changes to a database asynchronously.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>
+        ///   The number of affected rows.
+        /// </returns>
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Persistence/Context/WeatherForecastContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherForecastApp.Application.Entities.Interfaces;
+using WeatherForecastApp.Domain.Entities.Models;
+
+namespace WeatherForecastApp.Persistence.Context
+{
+    /// <summary>
+    /// The weather forecast database context.
+    /// </summary>
+    /// <seealso cref="DbContext" />
+    /// <seealso cref="IDomainContext{TEntity}" />
+    public sealed class WeatherForecastContext : DbContext, IDomainContext<WeatherForecast>
+    {
+        /// <inheritdoc cref="IDomainContext{TEntity}.Entities"/>
+        public System.Data.Entity.DbSet<WeatherForecast> Entities { get; set; } = null!;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherForecastContext"/> class.
+        /// </summary>
+        public WeatherForecastContext(DbContextOptions<WeatherForecastContext> options)
+            : base(options)
+        {
+        }
+
+        /// <inheritdoc cref="IDomainContext{TEntity}.SaveChangesAsync(CancellationToken)"/>
+        async Task<int> IDomainContext<WeatherForecast>.SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return await SaveChangesAsync(cancellationToken);
+        }
+    }
+}
