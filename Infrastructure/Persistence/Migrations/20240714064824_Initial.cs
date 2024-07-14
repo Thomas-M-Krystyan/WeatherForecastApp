4a23fba6ae2004ed94b92defacfd7570c3db9835
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-14 08:59:07 +0200

    Initial migration

---
+++ Infrastructure/Persistence/Migrations/20240714064824_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherForecastApp.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherForecast",
+                columns: table => new
+                {
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TempCelsius = table.Column<float>(type: "real", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherForecast", x => x.DateTime);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherForecast");
+        }
+    }
+}
