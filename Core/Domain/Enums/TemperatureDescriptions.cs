0e4d91c2af82a8b84da86323468fd679a1f88b54
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-14 10:14:14 +0200

    Add temperature descriptions

---
+++ Core/Domain/Enums/TemperatureDescriptions.cs
@@ -0,0 +1,21 @@
+﻿namespace WeatherForecastApp.Domain.Enums
+{
+    /// <summary>
+    /// Predefined descriptions of the temperature aspect of the weather.
+    /// </summary>
+    public enum TemperatureDescriptions
+    {
+        Unknown = 0,
+
+        Freezing = 1,
+        Bracing = 2,
+        Chilly = 3,
+        Cool = 4,
+        Mild = 5,
+        Warm = 6,
+        Balmy = 7,
+        Hot = 8,
+        Sweltering = 9,
+        Scorching = 10
+    }
+}
