e496f4079b8581596fb7df3a65ce0fb5719fe23f
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2024-07-14 05:59:34 +0200

    Add new class to implement IServiceHandler interface

---
+++ Core/Domain/Resolvers/ServiceHandler.cs
@@ -0,0 +1,9 @@
+﻿using WeatherForecastApp.Domain.Resolvers.Interfaces;
+
+namespace WeatherForecastApp.Domain.Resolvers
+{
+    /// <inheritdoc cref="IServiceHandler"/>
+    public sealed class ServiceHandler : IServiceHandler
+    {
+    }
+}
